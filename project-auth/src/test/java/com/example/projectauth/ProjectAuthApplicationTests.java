ab2b4a26ac5bdfbba106d909183c221e395597bc
SoonWookHwang <bravadosw@naver.com>
2023-01-31 22:23:27 +0900

    first setting

---
+++ project-auth/src/test/java/com/example/projectauth/ProjectAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
